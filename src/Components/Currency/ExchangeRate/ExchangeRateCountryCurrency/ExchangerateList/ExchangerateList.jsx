877377bf33255584b16bfae577fd741e63399b3e
nebeolisa chiemezie samson <daddygreenarrow44@gmail.com>
2024-05-26 14:58:53 +0900

    modified the exchange rate display design

--- src/Components/Currency/ExchangeRate/ExchangeRateCountryCurrency/ExchangerateList/ExchangerateList.jsx
+++ src/Components/Currency/ExchangeRate/ExchangeRateCountryCurrency/ExchangerateList/ExchangerateList.jsx
@@ -21,15 +21,15 @@ const ExchangerateList = ({countriesRate, lang, handleSetSearchResult, searchRes
       />
 
       <ul className='grid gap-x-4 max-w-4xl mx-auto p-1 overflow-x-scroll md:overflow-x-hidden pt-10'>
-        <li className='grid grid-cols-3 md:grid-cols-4 min-w-[488px] md:min-w-0 pl-1 sm:text-lg lg:text-xl font-poppins font-semibold uppercase pb-1.5'>
+        <li className='grid grid-cols-3 md:grid-cols-4 pl-1 sm:text-lg lg:text-xl font-poppins font-semibold uppercase pb-1.5'>
           <span className='col-span-1 hidden md:inline-block'>{NameTags.country[lang]}</span> 
           <span className='col-span-2'>{NameTags.currency_name_code[lang]}</span> 
-          <span className='col-span-1'>{NameTags.exchange_rate[lang]}</span> 
+          <span className='col-span-1 text-center xs:text-left'>{NameTags.exchange_rate[lang]}</span> 
         </li>
 
         {finalResult?.map((country, index) => {
           return (
-            <li key={country?.id + index} className='grid grid-cols-3 md:grid-cols-4 border-t-2 last:border-b-2 border-black dark:border-white min-w-[488px] md:min-w-0'>
+            <li key={country?.id + index} className='grid grid-cols-3 md:grid-cols-4 border-t-2 last:border-b-2 border-black dark:border-white'>
 
               <Link 
                 to={`/currency/exchange_rate/${country?.id}`}
@@ -44,23 +44,23 @@ const ExchangerateList = ({countriesRate, lang, handleSetSearchResult, searchRes
                 </span>
               </Link>         
 
-              <div className='col-span-2 grid grid-cols-5 relative p-1 border-l-2 md:border-0 border-black dark:border-white'>
+              <div className='col-span-2 text-center xs:text-left xs:grid xs:grid-cols-5 xs:relative p-1 border-l-2 md:border-0 border-black dark:border-white'>
                 <Link to={`/currency/exchange_rate/${country?.id}`} >
                   <img 
                     src={country?.flag?.svg ? country?.flag?.svg : country?.flag?.png} 
                     alt={country?.flag?.alt} 
                     loading="lazy"
-                    className='aspect-square max-h-16 col-span-1 hover:scale-[1.03] focus-within:scale-[1.03] transitionEffect'
+                    className='aspect-square max-h-16 max-w-32 mx-auto col-span-1 hover:scale-[1.03] focus-within:scale-[1.03] transitionEffect'
                   />
                 </Link>
                 
                 <span className='col-span-4 grid'>
-                  <span className='absolute right-1 top-0 text-amber-500 dark:text-green-500 text-balance max-w-[70%] text-right'>{country?.currency_name[lang]}</span>
-                  <h3 className='absolute bottom-0 font-poppins font-semibold capitalize sm:text-lg lg:text-xl mx-1'>{country?.currency_code}</h3>
+                  <h3 className='xs:absolute xs:bottom-0 font-poppins font-semibold capitalize mt-2.5 sm:text-lg lg:text-xl mx-1'>{country?.currency_code}</h3>
+                  <span className='xs:absolute xs:right-1 xs:top-0 text-amber-500 dark:text-green-500 text-balance mt-2.5 xs:text-right'>{country?.currency_name[lang]}</span>
                 </span>
               </div>
 
-              <div className='col-span-1 grid items-center justify-center p-1 dark:text-amber-500 text-green-500 text-xl border-x-2 border-black dark:border-white'>
+              <div className='col-span-1 grid items-center text-center xs:text-left justify-center p-1 dark:text-amber-500 text-green-500 text-xl border-x-2 border-black dark:border-white'>
                 <span><span>{country?.currency_symbol}</span> {country?.conversion_rates}</span> 
               </div>
             </li>
